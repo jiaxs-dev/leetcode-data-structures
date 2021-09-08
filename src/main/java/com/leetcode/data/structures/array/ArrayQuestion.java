146b1adb67eacf7fd95500898c9c74ed96a33072
jiaxun19920218 <2572104454@qq.com>
2021-09-08 14:42:45 +0800

    day1 -  array

---
+++ src/main/java/com/leetcode/data/structures/array/ArrayQuestion.java
@@ -0,0 +1,16 @@
+package com.leetcode.data.structures.array;
+
+/**
+ * @description
+ * @author jiaxiansun
+ * @createTime 2021/9/8 14:41
+ * @version 1.0
+ */
+public class ArrayQuestion {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
